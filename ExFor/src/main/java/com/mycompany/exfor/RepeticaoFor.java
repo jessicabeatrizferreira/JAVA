5bce78f56d00dcd94b532f3399ff2b68edc98888
jessicabeatrizferreira <jessicabaf@gmail.com>
2023-04-06 07:57:12 -0300

    add exercicio de java usando for

---
+++ ExFor/src/main/java/com/mycompany/exfor/RepeticaoFor.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.exfor;
+
+/**
+ *
+ * @author Jéssica Beatriz
+ */
+public class RepeticaoFor {
+    public static void main(String[] args) {
+        for (int cc=0; cc<=3; cc++){
+            System.out.println("CAMBALHOTA");
+        }
+    }
+}
