1c6fb6b2c13289eee8ae17c5a4c3e329461352d6
jessicabeatrizferreira <jessicabaf@gmail.com>
2023-04-10 08:22:55 -0300

    exercicio usando ifs aninhandos - switch

---
+++ TesteMesSwitch/src/main/java/com/mycompany/testemesswitch/TesteMesSwitch.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.testemesswitch;
+
+/**
+ *
+ * @author jessi
+ */
+public class TesteMesSwitch {
+
+    public static void main(String[] args) {
+        int mes = 10;
+        switch (mes) {
+            case 1:
+                System.out.println("O mes é Janeiro");
+                break;
+            case 2:
+                System.out.println("O mes é Fevereiro");
+                break;
+            case 3:
+                System.out.println("O mes é Março");
+                break;
+            case 4:
+                System.out.println("O mes é Abril");
+                break;
+            case 5:
+                System.out.println("O mes é Maio");
+                break;
+            case 6:
+                System.out.println("O mes é Junho");
+                break;
+            case 7:
+                System.out.println("O mes é Julho");
+                break;
+            case 8:
+                System.out.println("O mes é Agosto");
+                break;
+            case 9:
+                System.out.println("O mes é Setembro");
+                break;
+            case 10:
+                System.out.println("O mes é Outubro");
+                break;
+            case 11:
+                System.out.println("O mes é Novembro");
+                break;
+            case 12:
+                System.out.println("O mes é Dezembro");
+                break;
+            default:
+                System.out.println("Mês Inválido!");
+                break;
+
+        }
+    }
+}
