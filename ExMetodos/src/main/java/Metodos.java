b785c7dc7241a8cfaaf679127cb55a7ac5e280ae
jessicabeatrizferreira <jessicabaf@gmail.com>
2023-04-11 12:15:35 -0300

    Add exemplo usando métodos

---
+++ ExMetodos/src/main/java/Metodos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jéssica Beatriz
+ */
+public class Metodos {
+    static int soma (int a, int b) {
+        int s = a+b;
+        return s;
+}
+    public static void main(String[] args) {
+       int sm = soma(5,2);
+        System.out.println(sm);
+    }
+ 
+}
