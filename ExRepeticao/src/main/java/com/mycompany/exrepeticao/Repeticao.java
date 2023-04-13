7332fffe96393a96bd9139d77168f6e31bc24345
jessicabeatrizferreira <jessicabaf@gmail.com>
2023-04-13 08:07:40 -0300

    add ex usando repetições

---
+++ ExRepeticao/src/main/java/com/mycompany/exrepeticao/Repeticao.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.exrepeticao;
+
+/**
+ *
+ * @author Jéssica Beatriz
+ */
+public class Repeticao {
+    public static void main(String[] args) {
+        int cc=0;
+        do {
+            System.out.println(cc);
+            cc++;
+        } while (cc<4);
+    }
+    
+}
