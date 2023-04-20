37144ea0d7ba3db5873ccb6b95561cd23a9721c8
jessicabeatrizferreira <jessicabaf@gmail.com>
2023-04-20 10:29:03 -0300

    atualizando classe conta

--- Bytebank-alura/Bytebank/src/main/java/com/mycompany/bytebank/CriarConta.java
+++ Bytebank-alura/Bytebank/src/main/java/com/mycompany/bytebank/CriarConta.java
@@ -1,18 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.bytebank;
 
-/**
- *
- * @author jessi
- */
 public class CriarConta {
 
     public static void main(String[] args) {
         Conta primeiraConta = new Conta(); //primeira conta do tipo Conta recebe o que o new conta devolver
         primeiraConta.saldo = 200; //atribuiu um valor ao saldo da primeira conta. o ponto é a referencia do saldo da primeira conta.
+        System.out.println(primeiraConta.saldo);
+        
+        primeiraConta.saldo += 100;
         System.out.println(primeiraConta.saldo);
     }
 }
