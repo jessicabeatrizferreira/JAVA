68be923f8bab35baffa0fa05aecea9edb848f891
jessicabeatrizferreira <jessicabaf@gmail.com>
2023-04-20 10:28:04 -0300

    Add ex vetores

---
+++ ExVetores/src/main/java/com/mycompany/exvetores/Vetor.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.exvetores;
+
+/**
+ *
+ * @author Jéssica Beatriz
+ */
+public class Vetor {
+    public static void main(String[] args) {
+        int n[] ={3,2,8,7,5,4};
+        for (int c= 0; c<=5; c++){
+            System.out.print(n[c]);
+        }
+    }
+}
